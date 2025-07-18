fedc0d5f821ac593b544f1155437de86aeb4e38d
[dyad] <git@dyad.sh>
2025-07-18 16:00:35 +0200

    [dyad] Interface d’administration pour créer salariés et références - wrote 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import ReferenceSelection from "./pages/ReferenceSelection";
 import Recap from "./pages/Recap";
+import Admin from "./pages/Admin";
 
 const queryClient = new QueryClient();
 
@@ -20,6 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/references" element={<ReferenceSelection />} />
           <Route path="/recap" element={<Recap />} />
+          <Route path="/admin" element={<Admin />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
