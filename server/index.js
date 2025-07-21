3110694f6659a74a18f4b7cca19815da0ce509c5
[dyad] <git@dyad.sh>
2025-07-21 09:59:04 +0200

    [dyad] Correction : destructuring + Automizer.create() pour v0.5.0 - wrote 1 file(s) + extra files edited outside of Dyad

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-const Automizer = require("pptx-automizer");
+const { Automizer } = require("pptx-automizer"); // <-- Correction ici
 const cors = require("cors");
 
 const app = express();
@@ -37,7 +37,7 @@ app.post("/api/enrich-cv", upload.single("pptx"), async (req, res) => {
     }
 
     // Utilisation de l'API v0.5.0
-    const automizer = new Automizer()
+    const automizer = Automizer.create()
       .load(templatePath)
       .load(pptxPath)
       .write(outputPath);
